68757124b965df2e75991bf404ad84466d85096a
Torbjörn Lindquist <torbjorn.lindquist@konj.se>
2025-04-28 12:22:00 +0200

    Data och kod turfrapport april 2025

--- apianropallazoner.py
+++ apianropallazoner.py
@@ -4,12 +4,12 @@ import json
 #url="https://statistik.konj.se:443/PxWeb/api/v1/sv/KonjBar/indikatorer/Indikatorm.px"
 #query= "{'query': [{'code': 'Indikator', 'selection': {'filter': 'item', 'values': ['KIFI', 'BTOT']}}, {'code': 'Period', 'selection': {'filter': 'item', 'values': ['2022M11']}}], 'response': {'format': 'json'}}"
 
-url="https://api.turfgame.com/unstable/zones/all"
+url="https://api.turfgame.com/v5/zones/all"
 
 response = requests.get(url)
 print(response.status_code)
 #print(response.text)
 
-f = open("data/allzonesv5202410.json", "a", encoding='utf-8')
+f = open("data/allzonesv5202504.json", "a", encoding='utf-8')
 f.write(response.text)
 f.close()
